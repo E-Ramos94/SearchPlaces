a09242b9628fa4f6349b5d5c9bab9bc21bb83198
Enrique Ramos <kikeramos.1994@gmail.com>
2023-11-24 19:47:32 -0600

    primer commit

---
+++ app/src/main/java/com/enrique/searchplaces/MainActivity.java
@@ -0,0 +1,59 @@
+package com.enrique.searchplaces;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView dummyText = findViewById(R.id.dummyText);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "TU_API");
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                String nombre = place.getName().toString();
+                //LatLng latLng = place.getLatLng();
+                dummyText.setText(nombre);
+                Toast.makeText(MainActivity.this, nombre, Toast.LENGTH_SHORT).show();
+            }
+
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.e("An error occurred: ", status.getStatusMessage());
+            }
+        });
+    }
+}
